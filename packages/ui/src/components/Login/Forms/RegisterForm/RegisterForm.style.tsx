24bc91014876cd4bd55c3f480a557eb600d4ce57
Morgan Parry <mparry92@gmail.com>
2021-01-20 07:05:17 +0000

    center the register form

--- packages/ui/src/components/Login/Forms/RegisterForm/RegisterForm.style.tsx
+++ packages/ui/src/components/Login/Forms/RegisterForm/RegisterForm.style.tsx
@@ -5,3 +5,11 @@ export const StyledRegisterButton = styled(Button)`${({ theme }) => css`
     margin-top: ${theme.spacing(3)};
 `}
 `
+
+export const StyledContainer = styled.article`
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+`
--- packages/ui/src/components/Login/Forms/RegisterForm/RegisterForm.tsx
+++ packages/ui/src/components/Login/Forms/RegisterForm/RegisterForm.tsx
@@ -1,12 +1,15 @@
 import { TextInput } from "../../../common/TextInput"
 import { FormGroup, StyledForm } from "../LoginForm/LoginForm.style"
 import { useAuthorizeUser } from "../../../../hooks/useAuthorizeUser"
-import { StyledRegisterButton } from "./RegisterForm.style"
+import { StyledContainer, StyledRegisterButton } from "./RegisterForm.style"
+import { useToasts } from 'react-toast-notifications'
 
 export const RegisterForm = () => {
     const { handleChange, handleSubmit } = useAuthorizeUser('register')
+    const { addToast } = useToasts()
 
     return (
+        <StyledContainer>
         <StyledForm onSubmit={handleSubmit}>
         <FormGroup>
         <TextInput data-testid='signUp-email' onChange={handleChange('email')} label='Email' type='email' />
@@ -14,5 +17,6 @@ export const RegisterForm = () => {
         <StyledRegisterButton data-testid='signUp-btn' text='Sign up!' type='submit' />
         </FormGroup>
         </StyledForm>
+        </StyledContainer>
     )
 }
